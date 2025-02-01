91aec55ee0cf4bbac3b0d5f356dfe8e01eb5f9be
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-01 13:31:16 +0530

    intial server stared

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Server Started")
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser())
+
+app.listen(port,()=>{
+    console.log(`Server is running on Port: ${port}`);
+})
+
